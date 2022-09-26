300fd277c5a9031db4b25ec6e79280c72a821aba
Bethany Long <bethanylong.x90@gmail.com>
2022-09-25 17:29:36 -0700

    Meld the react photo-annotator and the python static-site-generator

---
+++ photo-annotator/src/App.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import './App.css';
+
+class PictureDetails extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            headline: this.props.details.headline || "",
+            description: this.props.details.description || "",
+            locate: this.props.details.locate || "",
+            tags: this.props.details.tags || ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        if (!props.details) return state;
+        return {
+            headline: props.details.headline || "",
+            description: props.details.description || "",
+            locate: props.details.locate || "",
+            tags: props.details.tags || ""
+        };
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+        this.props.updatefn(this.props.file.name, event.target.name, event.target.value);
+    }
+
+    render() {
+        return (
+            <div className="picture-details-grid">
+                <span>Name:</span> <span>{this.props.file.name}</span>
+                <span>Headline:</span> <textarea className="small-textarea" name="headline" autoComplete="off" value={this.state.headline} onChange={this.handleChange} />
+                <span>Description:</span> <textarea className="big-textarea" name="description" autoComplete="off" value={this.state.description} onChange={this.handleChange} />
+                <span>Location:</span> <textarea className="small-textarea" name="locate" autoComplete="off" value={this.state.locate} onChange={this.handleChange} />
+                <span>Tags:</span> <textarea className="small-textarea" name="tags" autoComplete="off" value={this.state.tags} onChange={this.handleChange} />
+            </div>
+        );
+    }
+}
+
+const Picture = (props) => {
+    return (
+        <React.Fragment>
+            <img src={props.blobUrl}></img>
+            <PictureDetails file={props.loadedFile} updatefn={props.updatefn} details={props.details} />
+        </React.Fragment>
+    );
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pictures: [],
+            details: {},
+            dirtyDetails: false  // 😳
+        };
+        this.loadPictures = this.loadPictures.bind(this);
+        this.saveDetails = this.saveDetails.bind(this);
+        this.loadDetails = this.loadDetails.bind(this);
+        this.updateDetails = this.updateDetails.bind(this);
+    }
+
+    async loadPictures(event) {
+        let newPictures = [];
+        const dirHandle = await window.showDirectoryPicker();
+        for await (const entry of dirHandle.values()) {
+            if (!entry.name.endsWith(".jpg")) {
+                continue;
+            }
+            const file = await entry.getFile();
+            newPictures.push(file);
+        }
+        newPictures = newPictures.sort((a, b) => {
+            if (a.name < b.name) return -1;
+            if (a.name > b.name) return 1;
+            return 0;
+        });
+        this.setState({pictures: newPictures});
+    }
+
+    async saveDetails() {
+        const output = JSON.stringify(this.state.details);
+        const options = {
+            //suggestedName: "details.json",
+            types: [{
+                description: "JSON Files",
+                accept: {
+                    "application/json": [".json"]
+                }
+            }]
+        };
+        const handle = await window.showSaveFilePicker(options);
+        const writeHandle = await handle.createWritable();
+        await writeHandle.write(output);
+        await writeHandle.close();
+        this.setState({dirtyDetails: false});
+    }
+
+    async loadDetails() {
+        const [handle] = await window.showOpenFilePicker();
+        const file = await handle.getFile();
+        const detailsText = await file.text();
+        this.setState({
+            details: JSON.parse(detailsText),
+            dirtyDetails: false
+        });
+        //this.forceUpdate();
+    }
+
+    updateDetails(file, key, value) {
+        this.setState(prevState => {
+            let details = Object.assign({}, prevState.details);
+            if (!details.hasOwnProperty(file)) {
+                details[file] = {};
+            }
+            details[file][key] = value;
+            return {
+                pictures: prevState.pictures,
+                details: details,
+                dirtyDetails: true
+            };
+        });
+    }
+
+    render() {
+        const pictures = this.state.pictures.map((loadedFile) =>
+            <Picture key={loadedFile.name} loadedFile={loadedFile} blobUrl={URL.createObjectURL(loadedFile)} details={this.state.details[loadedFile.name] || {}} updatefn={this.updateDetails} />
+        );
+        return (
+          <div className="App">
+              <button id="interact" disabled={this.state.pictures.length > 0} onClick={this.loadPictures}>Load photo folder</button>
+              {this.state.pictures.length > 0 &&
+              <>
+                  <button id="saveDetails" disabled={!this.state.dirtyDetails} onClick={this.saveDetails}>Save text</button>
+                  <button id="loadDetails" onClick={this.loadDetails}>Load text</button>
+              </>
+              }
+              <div id="pictures" className="pictures-grid">{pictures}</div>
+          </div>
+        );
+    }
+}
+
+export default App;
---
+++ static-site-generator/generate.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3.10
+
+import collections
+from jinja2 import Environment, FileSystemLoader
+import itertools
+import json
+import os
+import shutil
+import sys
+
+CSS_FILE = "site.css"
+INDEX_HTML = "index.html"
+TEMPLATE_DIR = "templates/"
+
+TOPICS = {
+    "engineering": {
+        "name": "engineering",
+        "slug": "engineering",
+        "image": "carbon-river-suspension-bridge-tower-cables.jpg",
+        "style": "background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('../images/carbon-river-suspension-bridge-tower-cables.jpg')",
+    },
+    "erosion": {
+        "name": "erosion",
+        "slug": "erosion",
+        "image": "burroughs-sign-erosion-windward.jpg",
+        "style": "background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('../images/burroughs-sign-erosion-windward.jpg')",
+    },
+    "glacier": {
+        "name": "glacier",
+        "slug": "glacier",
+        "image": "paradise-nisqually-river-headwaters.jpg",
+        "style": "background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('../images/paradise-nisqually-river-headwaters.jpg')",
+    },
+    "river": {
+        "name": "river",
+        "slug": "river",
+        "image": "carbon-river-boulders.jpg",
+        "style": "background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('../images/carbon-river-boulders.jpg')",
+    },
+    "subalpine meadow": {
+        "name": "subalpine meadow",
+        "slug": "subalpine-meadow",
+        "image": "indian-henry-wildflowers-and-cabin.jpg",
+        "style": "background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('../images/indian-henry-wildflowers-and-cabin.jpg')",
+    },
+    "temperate rainforest": {
+        "name": "temperate rainforest",
+        "slug": "temperate-rainforest",
+        "image": "carbon-river-tree-ferns.jpg",
+        "style": "background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('../images/carbon-river-tree-ferns.jpg')",
+    },
+    "tundra": {
+        "name": "tundra",
+        "slug": "tundra",
+        "image": "panhandle-gap-tundra.jpg",
+        "style": "background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('../images/panhandle-gap-tundra.jpg')",
+    },
+    "volcanism": {
+        "name": "volcanism",
+        "slug": "volcanism",
+        "image": "south-puyallup-columns-wide-view.jpg",
+        "style": "background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('../images/south-puyallup-columns-wide-view.jpg')",
+    },
+}
+
+
+def find_pictures_for_each_tag(pictures):
+    count = collections.Counter()
+    by_tag = collections.defaultdict(list)
+
+    for picture in sorted(pictures.keys()):
+        details = pictures[picture]
+        if "tags" in details:
+            tags = details["tags"].split(", ")
+
+            if not any(tag in tags for tag in ("photo", "panorama")):
+                raise ValueError("Item has no type like photo or panorama")
+
+            if "panorama" in tags:
+                # Skip panoramas for now
+                continue
+
+            for tag in TOPICS.keys():
+                if tag in tags:
+                    by_tag[tag].append(picture)
+
+            details["tags"] = tags
+            count.update(tags)
+        else:
+            # Untagged pictures are skipped (maybe they suck)
+            pass
+
+    return by_tag
+
+
+def write_to_path(parent_dir, filename, new_file_contents):
+    os.makedirs(parent_dir, exist_ok=True)
+    full_path = os.path.join(parent_dir, filename)
+    with open(full_path, "w") as fh:
+        fh.write(new_file_contents)
+
+
+def render_main_explore_page(topics=TOPICS):
+    env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+    tpl = env.get_template("explore_all.html")
+    content = tpl.render(topics=topics)
+    return content
+
+
+def render_explore_topic_page(lowercase_topic, picture_filenames, annotator_metadata):
+    topic = lowercase_topic.title()
+    pictures = []
+    for picture_filename in picture_filenames:
+        picture_info = annotator_metadata[picture_filename]
+        picture_info["slug"] = picture_filename.split(".")[0]
+        picture_info["style"] = f'background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(\'../../images/{picture_filename}\')'
+        pictures.append(picture_info)
+
+    env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+    tpl = env.get_template("explore_topic.html")
+    content = tpl.render(
+        topic=topic,
+        pictures=pictures,
+    )
+    return content
+
+
+def write_all_topic_pages(root, annotator_metadata, by_tag):
+    for lowercase_topic, picture_filenames in by_tag.items():
+        content = render_explore_topic_page(lowercase_topic, picture_filenames, annotator_metadata)
+        slug = "-".join(lowercase_topic.split())
+        topic_dir_path = os.path.join(root, "explore", slug)
+        write_to_path(topic_dir_path, INDEX_HTML, content)
+
+
+def render_view_page(slug, picture_filename, picture_metadata):
+    headline = picture_metadata["headline"]
+    try:
+        description = picture_metadata["description"].split("\n\n")
+    except KeyError:
+        description = [""]
+
+    env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+    tpl = env.get_template("view.html")
+    content = tpl.render(
+        headline=headline,
+        filename=picture_filename,
+        description=description,
+    )
+    return content
+
+
+def write_all_view_pages(root, annotator_metadata, all_used_pictures):
+    for picture_filename in all_used_pictures:
+        slug = picture_filename.split(".")[0]
+        picture_metadata = annotator_metadata[picture_filename]
+        content = render_view_page(slug, picture_filename, picture_metadata)
+        view_dir_path = os.path.join(root, "view", slug)
+        write_to_path(view_dir_path, INDEX_HTML, content)
+
+
+if __name__ == "__main__":
+    # Figure out which tag(s) each picture belongs to
+    annotator_json_path = sys.argv[1]
+    with open(annotator_json_path) as fh:
+        metadata = json.load(fh)
+    by_tag = find_pictures_for_each_tag(metadata)
+
+    # Generate the "Explore - pick a topic" page
+    static_site_root_path = sys.argv[2]
+    explore_dir = os.path.join(static_site_root_path, "explore")
+    main_explore_page = render_main_explore_page()
+    write_to_path(explore_dir, INDEX_HTML, main_explore_page)
+
+    # Generate the "Engineering", "Erosion", etc pages
+    write_all_topic_pages(static_site_root_path, metadata, by_tag)
+
+    # Write CSS file
+    css_path = os.path.join(static_site_root_path, CSS_FILE)
+    shutil.copyfile(CSS_FILE, css_path)
+
+    # Make images dir
+    orig_picture_dir_path = sys.argv[3]
+    new_picture_dir_path = os.path.join(static_site_root_path, "images")
+    os.makedirs(new_picture_dir_path, exist_ok=True)
+
+    # Copy over all used pictures
+    all_used_pictures = set(list(itertools.chain(*list(by_tag.values()))))
+    for picture in all_used_pictures:
+        orig_picture_path = os.path.join(orig_picture_dir_path, picture)
+        new_picture_path = os.path.join(new_picture_dir_path, picture)
+        shutil.copyfile(orig_picture_path, new_picture_path)
+
+    # Generate the individual picture pages for all used pictures
+    write_all_view_pages(static_site_root_path, metadata, all_used_pictures)
